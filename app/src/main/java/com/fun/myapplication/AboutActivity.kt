cbf08d7917c78c87b05b34cdf169872a7bad5647
Kriti Tyagi <kriti.tyagi@testbook.com>
2020-12-17 01:26:25 +0530

    Initial commit

--- app/src/main/java/com/fun/myapplication/AboutActivity.kt
+++ app/src/main/java/com/fun/myapplication/AboutActivity.kt
@@ -2,10 +2,33 @@ package com.`fun`.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        val doneButton=findViewById<Button>(R.id.done_button)
+        val nickname_display=findViewById<TextView>(R.id.nickname_text)
+        val nickname= findViewById<EditText>(R.id.Nickname_edit)
+
+        doneButton.setOnClickListener()
+        {           nickname.setVisibility(GONE)
+                    doneButton.setVisibility(GONE)
+                    val txt: String =nickname.getText().toString()
+                    nickname_display.setText("Hi "+txt)
+                    nickname_display.setVisibility(VISIBLE)
+
+
+        }
+
     }
+
+
+
 }
--- app/src/main/java/com/fun/myapplication/MainActivity.kt
+++ app/src/main/java/com/fun/myapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.`fun`.myapplication
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -10,32 +12,39 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImage : ImageView
-    private lateinit var rocketAnimation: AnimationDrawable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.btn1)
-        val aboutButton = findViewById<Button>(R.id.about_btn)
+        val aboutButton: Button = findViewById(R.id.about_btn)
+
         diceImage = findViewById(R.id.dice_image)
-        val rocketImage = diceImage.apply { setBackgroundResource(R.drawable.rocket_thrust)
-            rocketAnimation = background as AnimationDrawable }
+
         rollButton.setOnClickListener { rollDice() }
+        aboutButton.setOnClickListener()
+        { about()}
 
 
+
+
+    }
+
+    private fun about()
+    {         val intent = Intent(this, AboutActivity::class.java)
+              startActivity(intent)
     }
     private fun rollDice()
     {       val resultText : TextView = findViewById(R.id.txt)
             val randomInt = (1..6).random()
-
-            val drawableResource = when(randomInt)
+            val drawableResource= when(randomInt)
             {   1-> R.drawable.dice_1
                 2-> R.drawable.dice_2
-                3->R.drawable.dice_3
-                4->R.drawable.dice_4
-                5->R.drawable.dice_5
-                else->R.drawable.dice_6
+                3-> R.drawable.dice_3
+                4-> R.drawable.dice_4
+                5-> R.drawable.dice_5
+                else-> R.drawable.dice_6
             }
-        val result_text= when(randomInt)
+        val result_text=when(randomInt)
         {   1-> "Hard Luck!"
             2-> "Double Attack!"
             3-> "Triple Trouble!"
